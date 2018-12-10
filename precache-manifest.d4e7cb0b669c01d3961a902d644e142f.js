fec9580f1873810ee92a7ac28b8dbcf20d5c40fb
Matt Auretta <mauretta2@gmail.com>
2018-12-09 17:23:23 -0700

    Updates

---
+++ precache-manifest.d4e7cb0b669c01d3961a902d644e142f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/memory-game/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "66c34b29d278a5d14e76",
+    "url": "/memory-game/static/js/runtime~main.66c34b29.js"
+  },
+  {
+    "revision": "8150cbc7ebc1192e7305",
+    "url": "/memory-game/static/js/main.8150cbc7.chunk.js"
+  },
+  {
+    "revision": "839d06aa379dc0dbffa1",
+    "url": "/memory-game/static/js/1.839d06aa.chunk.js"
+  },
+  {
+    "revision": "8150cbc7ebc1192e7305",
+    "url": "/memory-game/static/css/main.786c80ab.chunk.css"
+  },
+  {
+    "revision": "3bda4c103f77fd9a574a1815f34ab8d8",
+    "url": "/memory-game/index.html"
+  }
+];
